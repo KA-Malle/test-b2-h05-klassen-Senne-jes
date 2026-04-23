4d903b5dd3110eb824fc1759e498b6ecbc5583ca
Senne-jes <sennejes@gmail.com>
2026-04-23 15:39:55 +0200

    testH5klasseSenneJesers

--- Test H5 Klasse Pokemon LEEG/Test H5 Klasse Pokemon LEEG/Form1.cs
+++ Test H5 Klasse Pokemon LEEG/Test H5 Klasse Pokemon LEEG/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Test_H5_Klasse_Pokemon_LEEG
 {
@@ -16,6 +17,54 @@ namespace Test_H5_Klasse_Pokemon_LEEG
         {
             InitializeComponent();
         }
+
+        string speler1;
+        string speler2;
+        int will;
+
+        // Instantie van ons object aanmaken
+        Speler _gez = new Speler();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _gez.Gezondheid = 100;
+
+        }
+
+
+        private void buttonValAan1_Click(object sender, EventArgs e)
+        {           
+                _gez.Gezondheid += decimal.Parse(labelGezondheid2.Text);
+                //textBox1.Text = _rek.Uitlezen();            
+        }
+
+        private void buttonValAan2_Click(object sender, EventArgs e)
+        {
+            _gez.Gezondheid += decimal.Parse(labelGezondheid1.Text);
+
+        }
+
+        private void buttonGenees1_Click(object sender, EventArgs e)
+        {
+            _gez.Gezondheid -= decimal.Parse(labelGezondheid2.Text);
+
+        }
+
+        private void buttonGenees2_Click(object sender, EventArgs e)
+        {
+            _gez.Gezondheid -= decimal.Parse(labelGezondheid1.Text);
+
+        }
+
+        private void buttonToonPos_Click(object sender, EventArgs e)
+        {
+            // Instantie aanmaken
+            Positie pos = new Positie("Positie speler1:" + Positie + "Positie speler2:" + Positie);
+            MessageBox.Show(pos.Naam, "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        // Instanties aanmaken
+        // Speler gebr = new Speler1();
+        // Speler2 gebr = new Speler2();
 
 
     }
